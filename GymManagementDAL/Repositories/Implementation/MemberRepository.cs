bbf447986b0590cb7d89a9abab4dc7613ab62b2c
AhmedEl-Metwally <ahmed.moh.elmetwally@gmail.com>
2025-10-08 17:59:50 +0300

    Repository Pattern

---
+++ GymManagementDAL/Repositories/Implementation/MemberRepository.cs
@@ -0,0 +1,35 @@
+﻿using GymManagementDAL.Data.Context;
+using GymManagementDAL.Entities;
+using GymManagementDAL.Repositories.Interfaces;
+
+namespace GymManagementDAL.Repositories.Implementation
+{
+    public class MemberRepository(GymDbContext _context) : IMemberRepository
+    {
+
+        public int AddMember(Member member)
+        {
+            _context.Members.Add(member);
+            return _context.SaveChanges();
+        }
+
+        public int DeleteMember(int id)
+        {
+            var member =_context.Members.Find(id);
+            if(member is null)
+                return 0;
+            _context.Members.Remove(member);
+            return _context.SaveChanges();                  
+        }
+
+        public IEnumerable<Member> GetAllMembers() => _context.Members.ToList();
+
+        public Member? GetMemberById(int id) => _context.Members.Find(id);
+      
+        public int UpdateMember(Member member)
+        {
+            _context.Members.Update(member);
+            return _context.SaveChanges();  
+        }
+    }
+}
---
+++ GymManagementDAL/Repositories/Interfaces/IMemberRepository.cs
@@ -0,0 +1,13 @@
+﻿using GymManagementDAL.Entities;
+
+namespace GymManagementDAL.Repositories.Interfaces
+{
+    public interface IMemberRepository
+    {
+        IEnumerable<Member> GetAllMembers();
+        Member? GetMemberById(int id);
+        int AddMember(Member member);
+        int UpdateMember(Member member);
+        int DeleteMember(int id);
+    }
+}
