82578ad7b25bf73fc634d8331e0b2fe0bedb56cb
AhmedEl-Metwally <ahmed.moh.elmetwally@gmail.com>
2025-10-09 17:54:47 +0300

    Generic Repository

---
+++ GymManagementDAL/Repositories/Implementation/GenericRepository.cs
@@ -0,0 +1,31 @@
+﻿using GymManagementDAL.Data.Context;
+using GymManagementDAL.Entities;
+using GymManagementDAL.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace GymManagementDAL.Repositories.Implementation
+{
+    public class GenericRepository<TEntity>(GymDbContext _context) : IGenericRepository<TEntity> where TEntity : BaseEntity, new()
+    {
+        public int Add(TEntity entity)
+        {
+            _context.Set<TEntity>().Add(entity);
+            return _context.SaveChanges();  
+        }
+
+        public int Delete(TEntity entity)
+        {
+            _context.Set<TEntity>().Remove(entity);
+            return _context.SaveChanges();
+        }
+
+        public IEnumerable<TEntity> GetAll() => _context.Set<TEntity>().AsNoTracking().ToList();
+
+        public TEntity? GetById(int id) => _context.Set<TEntity>().Find( id);
+        public int Update(TEntity entity)
+        {
+            _context.Set<TEntity>().Update(entity);
+            return _context.SaveChanges();
+        }
+    }
+}
---
+++ GymManagementDAL/Repositories/Interfaces/IGenericRepository.cs
@@ -0,0 +1,13 @@
+﻿using GymManagementDAL.Entities;
+
+namespace GymManagementDAL.Repositories.Interfaces
+{
+    public interface IGenericRepository<TEntity> where TEntity : BaseEntity, new()
+    {
+        TEntity? GetById(int id);
+        IEnumerable<TEntity> GetAll();
+        int Add(TEntity entity);
+        int Update(TEntity entity);
+        int Delete(TEntity entity);
+    }
+}
