3c78884eda05052fb0e7c4e85460bb17fc95658b
AhmedEl-Metwally <ahmed.moh.elmetwally@gmail.com>
2025-10-09 16:41:03 +0300

    Complete Repository Pattern

---
+++ GymManagementDAL/Repositories/Implementation/SessionRepository.cs
@@ -0,0 +1,31 @@
+﻿using GymManagementDAL.Data.Context;
+using GymManagementDAL.Entities;
+using GymManagementDAL.Repositories.Interfaces;
+
+namespace GymManagementDAL.Repositories.Implementation
+{
+    public class SessionRepository(GymDbContext _context) : ISessionRepository
+    {
+        public int AddSession(Session session)
+        {
+            _context.Sessions.Add(session);
+            return _context.SaveChanges();
+        }
+
+        public int DeleteSession(Session session)
+        {
+            _context.Sessions.Remove(session);
+            return _context.SaveChanges();
+        }
+
+        public IEnumerable<Session> GetAllSessions() => _context.Sessions.ToList();
+
+        public Session? GetSessionById(int id) => _context.Sessions.Find(id);
+
+        public int UpdateSession(Session session)
+        {
+            _context.Sessions.Update(session);
+            return _context.SaveChanges();  
+        }
+    }
+}
---
+++ GymManagementDAL/Repositories/Interfaces/ISessionRepository.cs
@@ -0,0 +1,13 @@
+﻿using GymManagementDAL.Entities;
+
+namespace GymManagementDAL.Repositories.Interfaces
+{
+    public interface ISessionRepository
+    {
+        Session? GetSessionById(int id);
+        IEnumerable<Session> GetAllSessions();
+        int AddSession(Session session);
+        int UpdateSession(Session session);
+        int DeleteSession(Session session);
+    }
+}
