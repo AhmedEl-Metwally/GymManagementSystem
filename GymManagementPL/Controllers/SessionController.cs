4e4a860e5ce4d2145454ae0d6da2d0737c9d7cd7
AhmedEl-Metwally <ahmed.moh.elmetwally@gmail.com>
2025-11-13 18:34:23 +0200

    Session Controller - Details Action

--- GymManagementPL/Controllers/SessionController.cs
+++ GymManagementPL/Controllers/SessionController.cs
@@ -10,5 +10,22 @@ namespace GymManagementPL.Controllers
             var session = _sessionService.GetAllSessions();
             return View(session);
         }
+
+        public ActionResult Details(int id) 
+        {
+            if (id <= 0)
+            {
+                TempData["ErrorMessage"] = "Id Cannot be negative or zero";
+                return RedirectToAction(nameof(Index));
+            }
+
+            var session = _sessionService.GetSessionById(id);
+            if(session is null)
+            {
+                TempData["ErrorMessage"] = "Session not found";
+                return RedirectToAction(nameof(Index));
+            }
+            return View(session);
+        }
     }
 }
