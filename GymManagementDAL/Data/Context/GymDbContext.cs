301f184ed0a7260950480caf4ab55e2799522812
AhmedEl-Metwally <ahmed.moh.elmetwally@gmail.com>
2025-11-29 20:19:43 +0200

    Idenity Tables - Migration

--- GymManagementDAL/Data/Context/GymDbContext.cs
+++ GymManagementDAL/Data/Context/GymDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using GymManagementDAL.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System.Reflection;
 
@@ -6,30 +7,36 @@ using System.Reflection;
 
 namespace GymManagementDAL.Data.Context
 {
-    public class GymDbContext(DbContextOptions<GymDbContext> options) : DbContext(options)
+    public class GymDbContext(DbContextOptions<GymDbContext> options) : IdentityDbContext<ApplicationUser>(options)
     {
-        //protected override void OnModelCreating(ModelBuilder modelBuilder)
-        //{
-        //    base.OnModelCreating(modelBuilder);
-        //    modelBuilder.ApplyConfigurationsFromAssembly(typeof(GymDbContext).Assembly);
-        //}
-
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
             modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            modelBuilder.Entity<ApplicationUser>
+                (
+                    entities => 
+                    {
+                        entities.Property(E => E.FirstName)
+                                .HasColumnType("varchar")
+                                .HasMaxLength(60);
+
+                        entities.Property(E => E.LastName)
+                                .HasColumnType("varchar")
+                                .HasMaxLength(60);
+                    }
+                );
         }
 
 
         public DbSet<Category> Categories { get; set; }
-        public DbSet<HealthRecord> HealthRecords  { get; set; }
+        public DbSet<HealthRecord> HealthRecords { get; set; }
         public DbSet<Member> Members { get; set; }
         public DbSet<MemberPlan> MemberPlans{ get; set; }
         public DbSet<MemberSession> MemberSessions{ get; set; }
         public DbSet<Plan> Plans{ get; set; }
         public DbSet<Session> Sessions{ get; set; }
         public DbSet<Trainer> Trainers{ get; set; }
-
     }
 }
  
